ee7618fc8293b6939b780a6ca124a88f81472bd9
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-22 16:36:48 +0900

    기본 프로젝트 세팅 및 목록 화면 구성중

---
+++ android/app/src/main/kotlin/com/example/flutter_book_list_03/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_book_list_03
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class ListScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('도서 목록 앱')
+      ),
+      body: Container()
+    );
+  }
+}
